94429904c9e7c2ff927c338387dbfe6d343b8d53
Viturin Züst <viturin.zuest@bluewin.ch>
2022-09-16 20:53:56 +0200

    added all box all offset tests

--- roomTest.py
+++ roomTest.py
@@ -41,7 +41,7 @@ def test_room_from_json(filename):
     return str(room), data['result']
 
 
-class RoomTest(unittest.TestCase):
+class SingleBoxReachableTests(unittest.TestCase):
     def test_is_number_5_reachable_in_a_10_room(self):
         (result, msg) = find_helper(Room(10), 5)
         self.assertEqual(True, result, msg)
@@ -58,6 +58,8 @@ class RoomTest(unittest.TestCase):
         (result, msg) = find_helper(Room(1000), 7)
         self.assertEqual(True, result, msg)
 
+
+class SingleBoxWithOffsetReachableTests(unittest.TestCase):
     def test_is_number_7_reachable_in_a_10_room_with_2_offset(self):
         (result, msg) = find_helper(Room(10), 7, 2)
         self.assertEqual(True, result, msg)
@@ -70,6 +72,8 @@ class RoomTest(unittest.TestCase):
         (result, msg) = find_helper(Room(10), 11, 12)
         self.assertEqual(False, result, msg)
 
+
+class AllBoxReachableTests(unittest.TestCase):
     def test_all_numbers_of_a_room_with_size_10(self):
         (result, msg) = find_all_numbers_in_room(Room(10))
         self.assertEqual(True, result, msg)
@@ -82,6 +86,8 @@ class RoomTest(unittest.TestCase):
         (result, msg) = find_all_numbers_in_room(Room(1000))
         self.assertEqual(True, result, msg)
 
+
+class AllBoxWithOffsetReachableTests(unittest.TestCase):
     def test_all_numbers_of_a_room_with_size_10_and_offset_4(self):
         (result, msg) = find_all_numbers_in_room(Room(10), 4)
         self.assertEqual(True, result, msg)
@@ -94,6 +100,8 @@ class RoomTest(unittest.TestCase):
         (result, msg) = find_all_numbers_in_room(Room(1000), 4)
         self.assertEqual(True, result, msg)
 
+
+class RoomPrintTests(unittest.TestCase):
     def test_room_print_of_a_room_with_size_10_and_seed_1(self):
         (result, truth) = test_room_from_json("roomPrintSize10Seed1")
         self.assertEqual(result, truth, "room size 10 seed 1 print test")
@@ -101,6 +109,20 @@ class RoomTest(unittest.TestCase):
     def test_room_print_of_a_room_with_size_20_and_seed_2(self):
         (result, truth) = test_room_from_json("roomPrintSize20Seed2")
         self.assertEqual(result, truth, "room size 20 seed 2 print test")
+
+
+class AllBoxAllOffsetReachableTests(unittest.TestCase):
+    def test_all_numbers_and_offsets_of_a_room_with_size_10(self):
+        (result, msg) = find_all_numbers_in_room_with_all_offsets(Room(10))
+        self.assertEqual(True, result, msg)
+
+    def test_all_numbers_and_offsets_of_a_room_with_size_100(self):
+        (result, msg) = find_all_numbers_in_room_with_all_offsets(Room(100))
+        self.assertEqual(True, result, msg)
+
+    def test_all_numbers_and_offsets_of_a_room_with_size_200(self):
+        (result, msg) = find_all_numbers_in_room_with_all_offsets(Room(200))
+        self.assertEqual(True, result, msg)
 
 
 if __name__ == '__main__':
